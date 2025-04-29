c4d88cce3406923fdd19f679a44c0716569a0abc
MrBauBey <batuyumben@gmail.com>
2025-04-29 03:36:10 +0300

    Module Update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,6 @@ module com.ayakkabi.aya1 {
 
     requires org.controlsfx.controls;
     requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
     requires javafx.controls;
     requires javafx.swing;
