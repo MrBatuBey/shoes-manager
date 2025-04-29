055f33c095b603b000c7f77eb44088d12ce46a6f
MrBauBey <batuyumben@gmail.com>
2025-04-29 03:23:38 +0300

    Github Action

---
+++ src/main/java/com/ayakkabi/aya1/Launcher.java
@@ -0,0 +1,10 @@
+package com.ayakkabi.aya1;
+
+import java.io.IOException;
+
+public class Launcher {
+    public static void main(String[] args) throws IOException {
+
+        Main.main(args);
+    }
+}
---
+++ src/main/java/com/ayakkabi/aya1/SystemDefaults.java
@@ -0,0 +1,7 @@
+package com.ayakkabi.aya1;
+
+public class SystemDefaults {
+
+    public static String CURRENT_VERSION = "v1.0.0";
+
+}
